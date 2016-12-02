3f9052a1f15494af85eaf65497fec4721116fecb
Daniel Raymond <Daraymond@macbook-pro-0880-daniel-raymond.local>
2016-12-02 15:17:44 -0500

    Converted all the maze options into arrays. Now I'm creating the loops to build it.

--- Daedalus/GameScene.swift
+++ Daedalus/GameScene.swift
@@ -10,11 +10,21 @@ import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    var pathX = 0
+    var pathY = 0
+    let path = SKSpriteNode(imageNamed: "Grey Square")
+    let barrier = SKSpriteNode(imageNamed: "Daedalus Barrier")
     let player = SKSpriteNode(imageNamed: "Daedalus Sprite")
-    var randomVariable = 1
-    var pathTracker: [Bool] = []
-    var path: SKSpriteNode?
     
+    let mazeOption1: [Bool] = [false, true, false, true, false, false, false, false, true, false, false, false, true, false, false, true, false, true, false, true, false, false, false, false, false, false, true, false, false, true, false, true, false, true, false, false, true, false, false, false, true, false, false, true, false, true, false, true, true]
+    
+    let mazeOption2: [Bool] = [false, false, false, true, false, false, false, false, true, false, false, false, true, false, false, true, true, true, true, true, false, false, false, false, false, false, true, false, false, true, false, true, false, true, false, false, true, true, false, true, true, false, false, true, false, false, false, false, true]
+    
+    let mazeOption3: [Bool] = [false, false, false, true, false, false, false, false, true, false, false, false, true, false, false, true, false, true, false, true, false, false, false, false, true, false, false, false, false, true, false, true, false, true, false, false, true, false, true, false, true, false, false, true, false, false, true, false, false]
+    
+    let mazeOption4: [Bool] = [false, true, false, true, true, false, true, false, false, true, false, false, false, true, false, true, false, false, true, false, false, false, false, true, false, false, true, false, false, true, false, true, false, false, true, false, false, false, true, true, false, true, false, true, false, false, false, false, true]
+    
+    let mazeOption5: [Bool] = [false, true, false, false, false, false, false, false, false, true, false, true, false, true, false, true, false, false, true, false, false, false, false, true, false, false, true, false, false, true, false, true, false, false, true, false, false, false, true, true, true, false, false, true, false, false, false, false, false]
     
     override func didMove(to view: SKView) {
         // 3
@@ -22,132 +32,56 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // 4
         player.zPosition = 0
         addChild(player)
-        player.setScale(0.02)
+        player.setScale(0.05)
         
         //Tells the game whether there will be gravity
         physicsWorld.gravity = CGVector.zero
         //Make this class the physics contact delegate
         physicsWorld.contactDelegate = self
         
-        let background = SKSpriteNode(imageNamed: "Grey Square")
+        //        let background = SKSpriteNode(imageNamed: "Grey Square")
+        //
+        //        background.position = CGPoint(x: 0, y: size.width / 2 - (size.width / 9))
+        //        background.zPosition = -1
+        //        background.setScale(5.12)
+        //        addChild(background)
         
-        background.position = CGPoint(x: size.width / 2, y: size.width / 2)
-        background.zPosition = -1
-        background.size = self.frame.size
-        addChild(background)
+        // Add the first sprite
         
+        for CGFloat(repeatX) in mazeOption1 {
+        
+        let tile00 = SKSpriteNode(imageNamed: "Daedalus Barrier")
+        tile00.anchorPoint = CGPoint(x: repeatX * (size.width/9), y: 0)
+        tile00.position = CGPoint(x: 0, y: 0)
+        tile00.setScale(0.36833)
+        addChild(tile00)
+            
+        }
+        
+        let tile02 = SKSpriteNode(imageNamed: "Grey Square")
+        tile02.anchorPoint = CGPoint(x: 0, y: 0)
+        tile02.position = CGPoint(x: 0, y: (size.width / 9) * 2)
+        tile02.setScale(0.15333)
+        addChild(tile02)
     }
     // The function to create a path
     func addpath() {
         
-        path = SKSpriteNode(imageNamed: "Grey Square")
-        
         path.position = CGPoint(x: pathX, y: pathY)
         path.zPosition = 0
         addChild(path)
-        path.setScale(0.02)
+        path.setScale(0.5688888888)
     }
     
     // The function to create a barrier
     func addbarrier() {
         
-        let barrier = SKSpriteNode(imageNamed: "Daedalus Barrier")
-        
         barrier.position = CGPoint(x: size.width / 2, y: size.width / 2)
         barrier.zPosition = 0
         addChild(barrier)
-        barrier.setScale(0.02)
+        barrier.setScale(1.36533333333)
     }
-    // The func that creates the maze
-    func makeMaze() {
-        
-        // The randomizer to create a possible path to the end
-        
-        var pathX: CGFloat = 0
-        var pathY = size.height / 2
-        var heightOfPathCheck: CGFloat = 0
-        
-        for _ in 1...30 {
-            
-            if pathX < (size.width * 7 / 9) - 1 {
-                
-                let randomizer = arc4random_uniform(6) // Randomizer: different path options to place
-                
-                if pathY < size.height/2 + size.width/2 && pathY > size.height/2 - size.width/2 {
-                   // Create a path square on top of the previous one
-                    if randomizer == 0 {
-                        
-                        pathY = pathY + size.width / 9
-                        addpath()
-                        heightOfPathCheck += 1
-                        // Create two path squares on top of the previous one
-                    } else if randomizer == 1 {
-                        
-                        for _ in 1...2 {
-                            pathY = pathY + size.width / 9
-                            addpath()
-                        }
-                        
-                        heightOfPathCheck += 2
-                        
-                        // Create a path square to the right of the previous one
-                    } else if randomizer == 2 {
-                        
-                        pathX = pathX + size.width / 9
-                        addpath()
-                        // Create two path squares to the right of the previous one
-                    } else if randomizer == 3 {
-                        
-                        for _ in 1...2 {
-                            pathX = pathX + size.width / 9
-                            addpath()
-                        }
-                        // Create a path square below of the previous one
-                    } else if randomizer == 4 {
-                        
-                        pathY = pathY - size.width / 9
-                        addpath()
-                        
-                        heightOfPathCheck -= 1
-                        
-                    } else if randomizer == 5 {
-                        // Create two path squares below of the previous one
-                        for _ in 1...2 {
-                            pathY = pathY - size.width / 9
-                            addpath()
-                        }
-                        heightOfPathCheck -= 2
-                    }
-                    // If last column & above exit, go down.
-                } else if pathY - size.height > heightOfPathCheck {
-                    
-                    pathY = pathY - size.width / 9
-                    addpath()
-                    // If last column & below exit, go up.
-                } else if pathY - size.height < heightOfPathCheck {
-                    
-                    pathY = pathY + size.width / 9
-                    addpath()
-                    
-                } // If Y Bracket
-            } // Check to make sure it stays within the parameters of the game screen.
-        } // Loop Bracket
-        
-        // Generate blocks and paths around the path to fill the area
-        for generatedSpace in 1...20 {
-            
-            let blockChoice = arc4random_uniform(2)
-            
-            if blockChoice == 0 {
-                pathX = (CGFloat(generatedSpace) - 1) * 20
-                pathY = (generatedSpace - 1) * 20
-                addpath()
-            } else {
-                pathX = (generatedSpace - 1) * 20
-                pathY = (generatedSpace - 1) * 20
-                addbarrier()
-            }
-            
-        } // GeneratedSpace Loop Bracket
-    } // func makeMaze Bracket
+    
+    
+    
 } // Game Scene Class Bracket
