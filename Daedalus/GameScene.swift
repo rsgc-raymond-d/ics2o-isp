f58b779cff927daf072b0e04c3356e7a5896cdc8
Daniel Raymond <Daraymond@macbook-pro-0880-daniel-raymond.local>
2016-12-12 16:15:38 -0500

    All the sprites have been autoscaled (except for the title and the loading screen)

--- Daedalus/GameScene.swift
+++ Daedalus/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-
+    
     let path = SKSpriteNode(imageNamed: "Grey Square")
     let barrier = SKSpriteNode(imageNamed: "Daedalus Barrier")
     let player = SKSpriteNode(imageNamed: "Daedalus Sprite")
@@ -82,6 +82,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // Inner maze constructing
     func createMaze() {
         
+        let blockSize = size.width/9
+        let yBlockSize = size.height/2 - size.width/2
+        
         // Randomizer to create a number
         let mazeChoice = arc4random_uniform(7)
         var combineCoordinate = -1
@@ -107,6 +110,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         for repeatY in 1...7 {
             for repeatX in 1...7 {
                 
+                let blockXPosition = CGFloat(repeatX) * blockSize
+                let blockYPosition = -CGFloat(repeatY) * blockSize + size.height/2 + size.width/2 - blockSize
+                
                 combineCoordinate += 1
                 
                 if realMazeOption[combineCoordinate] == true {
@@ -114,16 +120,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     
                     barrier.name = "barrier"
                     barrier.anchorPoint = CGPoint(x: 0, y: 0)
-                    barrier.position = CGPoint(x: (repeatX) * (46), y: (-(repeatY) * 46) + 529)
-                    barrier.setScale(0.368333333)
+                    barrier.position = CGPoint(x: blockXPosition, y: blockYPosition)
+                    barrier.setScale(size.width/1125)
                     
                     addChild(barrier)
                 } else {
                     let path = SKSpriteNode(imageNamed: "Grey Square")
                     
                     path.anchorPoint = CGPoint(x: 0, y: 0)
-                    path.setScale(0.153333333)
-                    path.position = CGPoint(x: (repeatX) * (46), y: (-(repeatY) * 46) + 529)
+                    path.setScale(size.width/2700)
+                    path.position = CGPoint(x: blockXPosition, y: blockYPosition)
                     
                     addChild(path)
                 }}} // Inner maze finish bracket
@@ -131,12 +137,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // Top and Bottom Walls
         for lowerUpperWalls in 1...2 {
             for hWall in 1...9 {
+                let blockYPosition = blockSize * 8 * CGFloat(lowerUpperWalls - 1) + yBlockSize
+                
                 let barrier = SKSpriteNode(imageNamed: "Daedalus Barrier")
                 barrier.name = "barrier"
                 barrier.anchorPoint = CGPoint(x: 0, y: 0)
-                barrier.setScale(0.368333333)
+                barrier.setScale(size.width/1125)
                 addChild(barrier)
-                barrier.position = CGPoint(x: (hWall - 1) * (46), y: (368 * (lowerUpperWalls - 1)) + 161)
+                barrier.position = CGPoint(x: (CGFloat(hWall - 1)) * (blockSize), y: blockYPosition)
             }}
         
         // Side Walls
@@ -144,31 +152,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             for bothWalls in 1...7 {
                 
                 if bothWalls != 4 {
+                    let blockYPosition = size.height/2 + size.width/2 - (CGFloat(bothWalls) - 1) * blockSize - blockSize * 2
+                    
                     let barrier = SKSpriteNode(imageNamed: "Daedalus Barrier")
                     barrier.name = "barrier"
                     barrier.anchorPoint = CGPoint(x: 0, y: 0)
-                    barrier.setScale(0.368333333)
+                    barrier.setScale(size.width/1125)
                     addChild(barrier)
-                    barrier.position = CGPoint(x: (sideWalls - 1) * 368, y: 483 - ((bothWalls - 1) * 46))
+                    barrier.position = CGPoint(x: (CGFloat(sideWalls) - 1) * blockSize * 8, y: blockYPosition)
                 }}}
         
         // Starting and ending pathways
         
         let startPath = SKSpriteNode(imageNamed: "Grey Square")
         startPath.anchorPoint = CGPoint(x: 0, y: 0)
-        startPath.setScale(0.153333333)
-        startPath.position = CGPoint(x: 0, y: 345)
+        startPath.setScale(size.width/2700)
+        startPath.position = CGPoint(x: 0, y: size.height/2 - blockSize/2)
         addChild(startPath)
         
         let endPath = SKSpriteNode(imageNamed: "Grey Square")
         endPath.name = "endPath"
-        endPath.anchorPoint = CGPoint(x: 0, y: 0)
-        endPath.setScale(0.153333333)
-        endPath.position = CGPoint(x: 368, y: 345)
+        startPath.anchorPoint = CGPoint(x: 0, y: 0)
+        endPath.setScale(size.width/2700)
+        endPath.position = CGPoint(x: blockSize * 8 + blockSize/2, y: size.height/2)
         addChild(endPath)
         
         
-        // print(mazeChoice)
+        print(mazeChoice)
         
     } // Creating the maze ending brackets
     
@@ -184,14 +194,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         flashlight.position = CGPoint(x: size.width / 18, y: size.height / 2)
         flashlight.zPosition = 2
         addChild(flashlight)
-        flashlight.setScale(1.4)
+        flashlight.setScale(size.width*1.5/size.width)
     }
     // Function that adds the enemy
     func addEnemy() {
         let enemy = SKSpriteNode(imageNamed: "Enemy")
         enemy.position = CGPoint(x: size.width * 17 / 18, y: size.height / 2)
         enemy.zPosition = 1
-        addChild(enemy)
+        // addChild(enemy)
         enemy.setScale(0.01916666667)
         
         moveEnemyLeft()
@@ -250,9 +260,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         completionLabel.text = String(mazesCompleted)
         completionLabel.fontColor = SKColor.brown
-        completionLabel.fontSize = 150
+        completionLabel.fontSize = size.width/6
         completionLabel.zPosition = 5
-        completionLabel.position = CGPoint(x: size.width - size.width/8, y: size.height / 9)
+        completionLabel.position = CGPoint(x: size.width - size.width/6, y: size.height / 9)
         addChild(completionLabel)
         
     } // DidMove Closing Bracket
@@ -273,8 +283,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Creates a new maze
         createMaze()
-
+        
+        
         mazesCompleted += 1
+        completionLabel.text = String(mazesCompleted/3)
         addChild(completionLabel)
     }
     
@@ -311,14 +323,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func update(_ currentTime: TimeInterval) {
         // Check for collisions between the player and barriers
         checkCollisions()
-        
     }
     
     // This function checks for collisions
     func checkCollisions() {
         
         // The array of all the hitable objects in the game
-        var hitBarriers: [SKSpriteNode] = []
+        var playerHitBarriers: [SKSpriteNode] = []
+        var enemyHitBarriers: [SKSpriteNode] = []
         var endWin: [SKSpriteNode] = []
         
         // Finds all the obstacles
@@ -330,10 +342,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             // Check for obstacle intersection
             if barrier.frame.intersects(self.player.frame) {
-                
                 // what to do if they intersect
-                hitBarriers.append(barrier)
+                playerHitBarriers.append(barrier)
             }
+            
+            if barrier.frame.intersects(self.enemy.frame) {
+                // what to do if they intersect
+                enemyHitBarriers.append(barrier)
+            }
+            
         })
         
         enumerateChildNodes(withName: "endPath", using: {
@@ -348,9 +365,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
         })
         
-        // Loop for all th obstacles colliding with santa
-        for barrier in hitBarriers {
+        // Loop for all th obstacles colliding with things
+        for barrier in playerHitBarriers {
             playerHit(by: barrier)
+        }
+        
+        for barrier in enemyHitBarriers {
+            enemyHit(by: barrier)
         }
         
         for endPath in endWin {
@@ -363,6 +384,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.removeAction(forKey: "barrierStop")
         flashlight.removeAction(forKey: "barrierStop")
         
+    }
+    
+    func enemyHit(by barrier: SKSpriteNode) {
+        
+        let enemyChoice = arc4random_uniform(4)
+        if enemyChoice == 0 {
+            
+        } else if enemyChoice == 1 {
+            moveEnemyLeft()
+        } else if enemyChoice == 2 {
+            moveEnemyRight()
+        } else if enemyChoice == 3 {
+            moveEnemyUp()
+        } else if enemyChoice == 4 {
+            moveEnemyDown()
+        }
     }
     
     func playerWin(by endPath: SKSpriteNode) {
